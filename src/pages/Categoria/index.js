970b952e2488a9a5cb0c2f8a0792efcce7fbf86f
ROBERTO PACHECO CLARO <roberto.claro@viavarejo.com.br>
2024-01-31 15:43:30 -0300

    Criando componente Item

--- src/pages/Categoria/index.js
+++ src/pages/Categoria/index.js
@@ -1,10 +1,17 @@
 import Header from 'components/Header';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import styles from './Categoria.module.scss';
+import Item from 'components/Item';
 
 export default function Categoria() {
   const { nomeCategoria } = useParams(); // Pegando o parâmetro da URL
-  const categoria = useSelector(state => state.categorias.find(categoria => categoria.id === nomeCategoria)); // Pegando a categoria do estado
+  const { categoria, itens } = useSelector(state => ({
+    categoria: state.categorias.find(categoria => categoria.id === nomeCategoria), // Pegando a categoria do estado
+    itens: state.itens.filter(item => item.categoria === nomeCategoria), // Pegando os itens do estado
+  }));
+
+
   return (
     <div>
       <Header
@@ -12,6 +19,11 @@ export default function Categoria() {
         descricao={categoria.descricao}
         imagem={categoria.header}
       />
+      <div className={styles.itens}>
+        {itens?.map(item => (
+          <Item key={item.id} {...item}/>
+        ))}
+      </div>
     </div>
   )
 }
